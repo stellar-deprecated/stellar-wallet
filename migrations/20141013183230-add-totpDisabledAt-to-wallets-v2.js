9e3ee9b932ffbb16da88eaa18a65b5c675902b63
Scott Fleckenstein <nullstyle@gmail.com>
2014-10-13 11:37:17 -0700

    Adds totpDisabledAt column

---
+++ migrations/20141013183230-add-totpDisabledAt-to-wallets-v2.js
@@ -0,0 +1,10 @@
+var dbm = require('stex').dbMigrate;
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.addColumn('wallets_v2', 'totpDisabledAt', { type: type.DATE_TIME }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.removeColumn('wallets_v2', 'totpDisabledAt', { type: type.DATE_TIME }, callback);
+};
